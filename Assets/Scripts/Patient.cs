894d82c36b9da1b6e194d09a226b5f9df30fb41c
unknown <omri.hopson@gmail.com>
2020-03-19 03:51:42 -0300

    added scriptable object for statemanegement

---
+++ Assets/Scripts/Patient.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Patient", menuName = "Patient")]
+public class Patient : ScriptableObject
+{
+    public string id;
+    public string firstName;
+    public string lastName;
+    public string age;
+    public string gender;
+    public string doctorNotes;
+    public string diagnosis;
+    public string allergies;
+    public string scans;
+    public string profilePicture;
+}
---
+++ Assets/Scripts/PatientMenuDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatientMenuDisplay : MonoBehaviour
+{
+    public Patient patient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(patient.firstName);
+    }
+}
