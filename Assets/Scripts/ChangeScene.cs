fd3c3fafdf67204f3ccd481fe91d89f8bb024e0a
unknown <omri.hopson@gmail.com>
2020-04-07 01:50:28 -0300

    added xray button click component

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeScene : MonoBehaviour
+{
+    public GameObject button;
+    void GoToScene()
+    {
+        //scene = sceneName;
+        GameObject newObj;
+        newObj = (GameObject)Instantiate(button, transform);
+        newObj.GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene("CtScene"));
+    }
+}
